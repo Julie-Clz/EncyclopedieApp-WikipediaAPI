fe85d559625bf6c58aa80daea46dcebc4601bd75
julie collazos <juliecollazos@hotmail.com>
2023-03-08 15:01:42 +0100

    Initial Commit

---
+++ Encyclopedie/EncyclopedieApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncyclopedieApp.swift
+//  Encyclopedie
+//
+//  Created by Julie Collazos on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct EncyclopedieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
